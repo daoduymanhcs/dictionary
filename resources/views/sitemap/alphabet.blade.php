d6847031e449802964732bf9dae65fc9aa438613
DAO DUY MANH <Mark@DAOs-MacBook-Air.local>
2018-03-20 23:29:58 +0700

    update

--- resources/views/sitemap/alphabet.blade.php
+++ resources/views/sitemap/alphabet.blade.php
@@ -3,7 +3,7 @@
   xmlns:xhtml="http://www.w3.org/1999/xhtml">
     @foreach ($data as $word)
         <url>
-            <loc>{{env('APP_URL', 'Laravel')}}/{!!html_entity_decode(preg_replace('/&(?!#?[a-z0-9]+;)/', '&amp;', $word->core_name))!!}</loc>
+            <loc>{{env('APP_URL', 'Laravel')}}/{!!html_entity_decode($word->core_name)!!}</loc>
             <lastmod>{{ $word->updated_at->tz('UTC')->toAtomString() }}</lastmod>
             <priority>0.8</priority>
         </url>
